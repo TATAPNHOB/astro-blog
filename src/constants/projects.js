53a963a9f5d11ee82a64d0fada2b3097e6ffefdd
TATAPNHOB <dantatarinov@gmail.com>
2023-09-25 00:00:05 +0300

    projects base, light rework

---
+++ src/constants/projects.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        "name": "coldopen",
+        "title": '"COLD OPEN"',
+        "description": "Why do they call it cold open when in out open cold food",
+        "img": "/evo-favicon.svg",
+        "link": "https://ttphb.itch.io/coldopen",
+        "cta": "Play it on itch.io"
+    }
+]
