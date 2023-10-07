bf7434d2c59ab71a01c5e41fcddaf01298c78e79
TATAPNHOB <dantatarinov@gmail.com>
2023-10-07 03:07:39 +0300

    welcome the projects page

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,10 +1,25 @@
 export const projects = [
     {
+        "name": "almostplayablecharacter",
+        "title": "ALMOST PLAYABLE CHARACTER",
+        "subtitle": "#343 OVERALL GMTK Game Jam Game",
+        "tags": ["shipped", "game", "gamejam", "coding"],
+        "finished": true,
+        "description": "Serving the entire playerbase in a (fictional) MMORPG is as hard as you'd think.",
+        "cta": "CHECK IT OUT ON ITCH.IO",
+        "link": "https://ttphb.itch.io/almostplayablecharacter",
+        "cover": "/apc_cover.png",
+        "front": "/stranger.png"
+    },
+    {
         "name": "coldopen",
-        "title": '"COLD OPEN"',
-        "description": "Why do they call it cold open when in out open cold food",
-        "img": "/evo-favicon.svg",
-        "link": "https://ttphb.itch.io/coldopen",
-        "cta": "Play it on itch.io"
+        "title": "\"COLD OPEN\"",
+        "subtitle": "UNIVERSITY ASSG. TWINE QUEST",
+        "tags": ["game", "unviersity", "solo", "unfinished"],
+        "finished": false,
+        "description": "Escape from weird bureaucratic underground nation -- with grace or otherwise.",
+        "cta": "NOT AVAILABLE YET",
+        "cover": "/coldopen_cover.png",
+        "front": "/statue.png"
     }
 ]
